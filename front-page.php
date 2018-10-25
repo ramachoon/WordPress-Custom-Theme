02e76e6977351f8d5a132826aed1dc9c14483dcb
Ramachandran <ramachoon@gmail.com>
2018-10-26 11:56:15 +1300

    added front-page.php to render the home page of the site

---
+++ front-page.php
@@ -0,0 +1,44 @@
+<!--
+    This file will render whatever page you set as your home page.
+    By default the home page is a list of all the posts but you can change it
+    to a static page through the customize section and the home page settings tab.
+-->
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <?php wp_head(); ?>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>Home Page</h1>
+                </div>
+                <?php if(have_posts()): ?>
+                    <div class="card-columns">
+                        <?php while(have_posts()): the_post();?>
+                            <div class="card">
+                                <?php if( has_post_thumbnail() ): ?>
+                                    <?php the_post_thumbnail('thumbnail', ['class'=>'card-img-top img-fluid', 'alt'=>'Card image cap']); ?>
+                                <?php endif; ?>
+                                <div class="card-body">
+                                    <h5 class="card-title"><?php the_title(); ?></h5>
+                                    <div class="">
+                                        <?php the_content(); ?>
+                                    </div>
+                                    <a class="btn btn-primary" href="<?= esc_url(get_permalink()); ?>">Go to post</a>
+                                </div>
+                            </div>
+                        <?php endwhile; ?>
+                    </div>
+                <?php endif; ?>
+
+            </div>
+        </div>
+        <?php wp_footer(); ?>
+    </body>
+</html>
