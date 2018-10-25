a0e652d9c8acf47ab0b4de5099d9e3b3db297f74
RichardHpaYoobee <richard.hpa@acgedu.com>
2018-10-26 11:56:15 +1300

    added front-page.php to render the home page of the site

--- indexalt.php
+++ indexalt.php
@@ -11,25 +11,22 @@
             <?php if(have_posts()): ?>
                 <?php while(have_posts()): the_post();?>
                     <div class="row">
-
-
-                            <?php if( has_post_thumbnail() ): ?>
-                                <?php $columnClass = 'col-xs-12 col-md-8'; ?>
-                                <div class="col-xs-12 col-md-4">
-                                    <?php the_post_thumbnail('medium', ['class'=>'img-fluid', 'alt'=>'thumbnail image']); ?>
-                                </div>
-                            <?php else: ?>
-                                <?php $columnClass = 'col-xs-12'; ?>
-                            <?php endif; ?>
-                                <div class="<?= $columnClass;  ?>">
-                                    <h3><?php the_title(); ?></h3>
-                                    <div><?php the_content(); ?></div>
-                                </div>
-
-
-
-
-
+                        <!--
+                            here is an alternative way to specifiy how many columns the second
+                            column needs depending on if there is an image or not
+                        -->
+                        <?php if( has_post_thumbnail() ): ?>
+                            <?php $columnClass = 'col-xs-12 col-md-8'; ?>
+                            <div class="col-xs-12 col-md-4">
+                                <?php the_post_thumbnail('medium', ['class'=>'img-fluid', 'alt'=>'thumbnail image']); ?>
+                            </div>
+                        <?php else: ?>
+                            <?php $columnClass = 'col-xs-12'; ?>
+                        <?php endif; ?>
+                        <div class="<?= $columnClass;  ?>">
+                            <h3><?php the_title(); ?></h3>
+                            <div><?php the_content(); ?></div>
+                        </div>
                     </div>
                 <?php endwhile; ?>
             <?php endif; ?>
