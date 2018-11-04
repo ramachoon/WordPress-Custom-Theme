e3c56d8c15ab3bda2a672b60b39ad083de661703
RichardHpaYoobee <richard.hpa@acgedu.com>
2018-11-05 10:48:24 +1300

    added comments into code

--- front-page.php
+++ front-page.php
@@ -34,9 +34,28 @@
                         </div>
                     <?php endif; ?>
                 </div>
+
+                <!--
+                    Our users don't actually need to include widgets on the theme.
+                    So we need to make sure we wrap it is around an if statement
+                    and if there aren't any widgets, then the design still needs to look good.
+
+                    In our theme. If there are widgets inside the front_page_sidebar then we want our theme
+                    to use 8 and 4 columns. Otherwise we want the main posts to take up the whole 12.
+
+                    is_active_sidebar() needs the ID of the registered sidebar. So this must match
+                    what you wrote in functions.php
+                -->
                 <?php if( is_active_sidebar('front_page_sidebar') ): ?>
                     <div class="col-4">
                         <div id="frontSidebar">
+                            <!--
+                                To get the sidebar to actually show all the widgets, then say dynamic_sidebar(id)
+                                This will then echo out all the widgets which you have specified in the widgets section.
+
+                                By default they will be echoed out as li's but if you change the befores and afters then
+                                it would use those instead.
+                            -->
                             <?php dynamic_sidebar('front_page_sidebar'); ?>
                         </div>
                     </div>
